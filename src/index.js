734476b9e58a23600ff24225ed9b2548fc166656
ashraf62069mustafa <116838514+ashraf62069mustafa@users.noreply.github.com>
2025-01-02 12:54:09 +0530

    firstCommit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n/i18n";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const language = i18n.language;
+const isRTL = ["ar", "fa"].includes(language);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <I18nextProvider i18n={i18n}>
+          <div className={isRTL ? "rtl" : "ltr"}>
+            <App />
+          </div>
+        </I18nextProvider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
